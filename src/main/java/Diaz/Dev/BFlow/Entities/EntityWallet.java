be85a34cc24ae3a5943a93a359972eec89ce81e3
Díaz <ed.diaz.hz@gmail.com>
2025-12-04 21:31:36 -0600

    More config

--- src/main/java/Diaz/Dev/BFlow/Entities/EntityWallet.java
+++ src/main/java/Diaz/Dev/BFlow/Entities/EntityWallet.java
@@ -1,12 +1,10 @@
 package Diaz.Dev.BFlow.Entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "TBWALLET")
 @Getter @Setter
 public class EntityWallet {
 }
