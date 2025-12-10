c5d1c32f3d23084cc6fc4771dc27ba0979334f60
Díaz <ed.diaz.hz@gmail.com>
2025-12-10 04:51:34 +0000

    Configuration almost completed

--- src/main/java/Diaz/Dev/BFlow/Entities/EntityWallet.java
+++ src/main/java/Diaz/Dev/BFlow/Entities/EntityWallet.java
@@ -1,10 +1,17 @@
 package Diaz.Dev.BFlow.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Table(name = "TBWALLET")
+@Getter @Setter @EqualsAndHashCode
 public class EntityWallet {
+    @Id @Column(name = "idWallet", columnDefinition = "SERIAL", insertable = false, updatable = false)
+    private Long idWallet;
 }
