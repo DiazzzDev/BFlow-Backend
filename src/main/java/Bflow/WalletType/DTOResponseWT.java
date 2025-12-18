8bd7a354e7f0d8d2e76bf57cd74fc610cdac0aee
Díaz <ed.diaz.hz@gmail.com>
2025-12-18 17:54:31 +0000

    Wallet type creation

---
+++ src/main/java/Bflow/WalletType/DTOResponseWT.java
@@ -0,0 +1,9 @@
+package Bflow.WalletType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DTOResponseWT {
+    
+}
---
+++ src/main/java/Bflow/WalletType/EntityWalletType.java
@@ -0,0 +1,13 @@
+package Bflow.WalletType;
+
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString @EqualsAndHashCode
+public class EntityWalletType {
+    
+}
