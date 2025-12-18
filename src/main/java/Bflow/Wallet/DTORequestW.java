86ab0212439fab7e8a2f405b3bde52caf41394cc
Díaz <ed.diaz.hz@gmail.com>
2025-12-18 17:19:37 +0000

    Wallet entity / DTO basic config

--- src/main/java/Bflow/Wallet/DTORequestW.java
+++ src/main/java/Bflow/Wallet/DTORequestW.java
@@ -1,5 +1,17 @@
 package Bflow.Wallet;
 
+import lombok.Getter;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Getter @Setter
 public class DTORequestW {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long idWallet;
     
+    private String name;
+
+    private Double value;
+
+    private Double initialValue;
 }
